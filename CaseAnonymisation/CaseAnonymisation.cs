fd52d35696f4f00f05b02e0112cbd666e97e1aa6
Landan Cerff <41990488+LandanJCerff@users.noreply.github.com>
2018-09-11 09:55:44 +0100

    Update CaseAnonymisation.cs

--- CaseAnonymisation/CaseAnonymisation.cs
+++ CaseAnonymisation/CaseAnonymisation.cs
@@ -1,14 +1,13 @@
-﻿using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Client;
-using Microsoft.Xrm.Sdk.Messages;
-using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
 using System;
-using System.IO;
+using System.Configuration;
+using System.Net;
 using System.ServiceModel;
-using System.ServiceModel.Description;
 using System.Threading;
-using System.Threading.Tasks;
+using System.ServiceModel.Description;
+using Microsoft.Xrm.Sdk.Client;
+using System.IO;
 
 namespace CaseAnonymisation
 {
@@ -19,51 +18,55 @@ namespace CaseAnonymisation
         static void Main(string[] args)
         {
             string env = args[0];
-  
-            FileStream ostrm;
-            StreamWriter writer;
-            ostrm = new FileStream("C:/CaseAnonApplication/CaseAnonymisation/Logs/Logs.txt", FileMode.OpenOrCreate, FileAccess.Write);
-            writer = new StreamWriter(ostrm);
+            //string env = "sandbox1";
+            IOrganizationService _orgServ = null;
+            IOrganizationService _orgServRes = null;
 
-            TextWriter oldOut = Console.Out;
-           
+            System.IO.FileStream ostrm;
+             StreamWriter writer;
+             ostrm = new FileStream("C:/CaseAnonApplication/CaseAnonymisation/Logs/Logs.txt", FileMode.OpenOrCreate, FileAccess.Write);
+             writer = new StreamWriter(ostrm);
+
+             TextWriter oldOut = Console.Out;   
+
             if (args.Length > 0)
             {
-                AuthenticationCredentials authCredentials = new AuthenticationCredentials();
-                Uri organizationUri = new Uri("https://" + env + ".api.crm4.dynamics.com/XRMServices/2011/Organization.svc");
-                authCredentials.ClientCredentials.UserName.UserName = ""; 
-                authCredentials.ClientCredentials.UserName.Password = ""; 
-                ClientCredentials credentials = authCredentials.ClientCredentials;
-                OrganizationServiceProxy proxy = new OrganizationServiceProxy(organizationUri, null, credentials, null);
-                proxy.Timeout = TimeSpan.FromMinutes(5000);
-                proxy.EnableProxyTypes();
-                IOrganizationService _orgServ = (IOrganizationService)proxy;
-                OrganizationServiceContext _orgContext = new OrganizationServiceContext(_orgServ);
+                ClientCredentials clientCredentials = new ClientCredentials();
+                clientCredentials.UserName.UserName = "";
+                clientCredentials.UserName.Password = "";
 
+                Console.WriteLine("credentials set");
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                _orgServ  = (IOrganizationService)new OrganizationServiceProxy(
+                    new Uri("https://" + env + ".api.crm4.dynamics.com/XRMServices/2011/Organization.svc"),
+                    null, clientCredentials,null);
+                _orgServRes = (IOrganizationService)new OrganizationServiceProxy(
+                    new Uri("https://" + env + ".api.crm4.dynamics.com/XRMServices/2011/Organization.svc"),
+                    null, clientCredentials, null);
 
-                Thread t1 = new Thread(() => mainThread1(_orgServ, "false"));          
-                Thread t2 = new Thread(() => mainThread2(_orgServ, "false"));
+                Thread t1 = new Thread(() => mainThread1(_orgServ, "false"));
+                Thread t2 = new Thread(() => mainThread2(_orgServRes, "false"));
                 Thread t3 = new Thread(() => mainThread3(_orgServ, "true"));
-           
 
                 try
                 {
                     Console.SetOut(writer);
                     Console.Write("Anonymisation Started... \n");
 
-                    t1.Start();                                 
-                    t2.Start();                  
+                   t1.Start();
+                   t2.Start();
                 }
 
                 catch (FaultException<OrganizationServiceFault> e)
-                {                  
+                {
                     Console.WriteLine("Cannot open Logs.txt for writing");
                     Console.WriteLine(e.Message);
                     Console.Write("Exception: {0} \n Exception occured ", e.ToString());
 
                     Console.SetOut(oldOut);
-                     writer.Close();
-                     ostrm.Close();                     
+                    writer.Close();
+                    ostrm.Close();
+                    
                     throw new InvalidPluginExecutionException(e.Message);
                     throw;
                 }
@@ -74,22 +77,21 @@ namespace CaseAnonymisation
         {
             Console.WriteLine("thread 1 initiated");
             ActiveCases(_orgServ, "false");
-            mainThread6(_orgServ, thread);
-        }       
+            mainThread3(_orgServ, thread);
+        }
        
-        static void mainThread2(IOrganizationService _orgServ, string thread)
+        static void mainThread2(IOrganizationService _orgServRes, string thread)
         {
-            Thread.Sleep(300000);
+            Thread.Sleep(10000);
             Console.WriteLine("thread 2 initiated");
-           ResolvedCases(_orgServ, "false");
+           ResolvedCases(_orgServRes, "false");
 
         }
-        
         static void mainThread3(IOrganizationService _orgServ, string thread)
         {
             Console.WriteLine("thread 3 initiated");            
             ResolvedCases(_orgServ, "true");
-        }
+        }       
     }
 }
 
