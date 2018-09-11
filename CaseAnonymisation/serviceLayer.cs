f18c1c8f52dbab99aa1dfae0d2aeda7448d8b4b8
Landan Cerff <41990488+LandanJCerff@users.noreply.github.com>
2018-09-11 09:56:13 +0100

    Update serviceLayer.cs

--- CaseAnonymisation/serviceLayer.cs
+++ CaseAnonymisation/serviceLayer.cs
@@ -1,12 +1,7 @@
-﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Client;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CaseAnonymisation
 {
@@ -14,88 +9,131 @@ namespace CaseAnonymisation
     {
         public static void ActiveCases(IOrganizationService _orgServ, string thread)
         {
-            OrganizationServiceContext _orgContext = new OrganizationServiceContext(_orgServ);
             string ticket = string.Empty;
+            string regarding = string.Empty;
         start:
             EntityCollection retrievedCases = GetActiveCases(_orgServ, thread);
 
             if (retrievedCases.Entities.Count != 0)
             {
                 try
-                {                   
-                    for (int i = 0; i < retrievedCases.Entities.Count; i++)
+                {
+                    foreach (Entity cas in retrievedCases.Entities)
                     {
-                        Incident myCase = (Incident)retrievedCases[i];
-                        ticket = myCase.TicketNumber;
+                        ticket = cas["ticketnumber"].ToString();
+                        DateTime createdOn = (DateTime)cas["createdon"];
+                        var casId = (Guid)cas["incidentid"];
 
-                        Console.Write("Case No.: " + ticket + " Updating... \n");
-                        myCase.Description = "XXXX XXXXXXXX, XXXX XXXX.";
-                        myCase.Title = "XXXXX XX. XXX XXXXX XX XXXX.";
-                        myCase.gcs_resolutiondescription = "XXXX XXXXXXX! XX XXXXX XXX; X XXXX XXX.";
+                        var updateCas = new Entity
+                        {
+                            Id = casId,
+                            LogicalName = "incident"
+                        };
 
+                        if (cas["gcs_casetypes"] == null)
+                        {
+                            _orgServ.Delete("incident", casId);
+                            continue;
+                        }
 
-                        _orgContext.Attach(myCase);
-                        _orgContext.UpdateObject(myCase);
-                        _orgContext.SaveChanges();
+                        regarding = cas["gcs_casetypes"].ToString();
 
-                        Console.WriteLine("\n Case " + ticket + " updated. \n");
+                        updateCas["description"] =
+                            "A case type of " + regarding + ", which was created on: " + createdOn;
 
-                        activityUpdate(_orgServ, myCase.Id);
+                        updateCas["title"] =
+                            "Anonymised Case: '" + regarding + "' Case created on: " + createdOn;
+
+                        updateCas["gcs_resolutiondescription"] =
+                            "A resolution case type of " + regarding + ", has been resolved.";
+
+                        activityUpdate(_orgServ, casId, ticket);
+
+                        _orgServ.Update(updateCas);
+
+                        Console.WriteLine(ticket + " updated. \n");
                     }
-                    _orgContext?.Dispose();
                     retrievedCases.Entities.Clear();
                 }
                 catch (FaultException<OrganizationServiceFault> e)
-                {                   
-                    Console.WriteLine("Exception: occured on Case: " + ticket + "\n");
-                    Console.WriteLine(e.Message);
+                {
+                    Console.WriteLine("Exception: occured on Case: " + ticket + "\n" + e.Message);
                 }
             }
             else
             {
                 return;
-            }                       
-                goto start;           
+            }
+            goto start;
         }
 
         public static void ResolvedCases(IOrganizationService _orgServ, string thread)
         {
             OrganizationServiceContext _orgContext = new OrganizationServiceContext(_orgServ);
-            string ticket = string.Empty;
-        start:
-            EntityCollection retrievedCases = GetResolvedCases(_orgServ, thread);
 
-            if (retrievedCases.Entities.Count != 0)
+            string ticket = string.Empty;
+            string regarding = string.Empty;
+        start:
+            EntityCollection retrievedResCases = GetResolvedCases(_orgServ, thread);
+
+            if (retrievedResCases.Entities.Count != 0)
             {
                 try
-                {                   
-                    for (int r = 0; r < retrievedCases.Entities.Count; r++)
+                {
+                    foreach (Entity cas in retrievedResCases.Entities)
                     {
-                        Incident myCase = (Incident)retrievedCases[r];
-                        int? statusReason = (int?)myCase.StatusCode.Value;
+                        var statusReason = cas.GetAttributeValue<OptionSetValue>("statuscode").Value;
+                        var dateClosed = cas.GetAttributeValue<DateTime>("gcs_dateresolved");
+                        var rootCause = cas.GetAttributeValue<EntityReference>("ccrm_rootcauseid");
+                        var resoCode = cas.GetAttributeValue<EntityReference>("gcs_resolutioncode");
+                        ticket = cas["ticketnumber"].ToString();
+                        DateTime createdOn = (DateTime)cas["createdon"];
 
-                        ticket = myCase.TicketNumber;
-                        Console.Write("Resolved Case No.: " + ticket + " Updating \n");
-                        myCase.Description = "XXXX XXXXXXXX, XXXX XXXX.";
-                        myCase.Title = "XXXXX XX. XXX XXXXX XX XXXX.";
-                        myCase.gcs_resolutiondescription = "XXXX XXXXXXX! XX XXXXX XXX; X XXXX XXX.";
-                        myCase.StateCode = 0;
-                        myCase.StatusCode = (incident_statuscode)1;
+                        var resCasId = (Guid)cas["incidentid"];
 
-                        _orgServ.Update(myCase);
-
-                        Entity incidentResolution = new Entity("incidentresolution");
-                        incidentResolution.Attributes.Add("incidentid", new EntityReference("incident", (Guid)myCase.Id));
-                        Console.WriteLine("\n Resolved Case " + ticket + " updated. \n");
-
-                        var closeIncidentRequest = new CloseIncidentRequest
+                        var updateResCas = new Entity
                         {
-                            IncidentResolution = incidentResolution,
-                            Status = new OptionSetValue((int)statusReason),
+                            Id = resCasId,
+                            LogicalName = "incident"
                         };
 
-                        activityUpdate(_orgServ, myCase.Id);
-                        CloseIncidentResponse closeResponse = (CloseIncidentResponse)_orgServ.Execute(closeIncidentRequest);
+
+                        if (cas["gcs_casetypes"] == null)
+                        {
+                            _orgServ.Delete("incident", resCasId);
+                        }
+
+                        regarding = cas["gcs_casetypes"].ToString();
+
+                        OpenResolvedCase(_orgServ, resCasId, ticket);
+
+
+                        updateResCas["description"] =
+                            "A case type of " + regarding + ", which was created on: " + createdOn;
+
+                        updateResCas["title"] =
+                            "Anonymised Case: " + regarding + " Case created on: " + createdOn;
+
+                        updateResCas["gcs_resolutiondescription"] =
+                            "A resolution case type of " + regarding + ", has been resolved.";
+
+                        if (dateClosed == default(DateTime))
+                        {
+                            dateClosed = DateTime.Now;
+                        }
+
+                        updateResCas["gcs_dateresolved"] = dateClosed.Date;
+                        updateResCas["ccrm_rootcauseid"] = rootCause;
+                        updateResCas["gcs_resolutioncode"] = resoCode;
+                        updateResCas["gcs_resolutiondescription"] = "Anonymised Closure";
+
+                        activityUpdate(_orgServ, resCasId, ticket);
+
+                        _orgServ.Update(updateResCas);
+
+                        CloseCase(_orgContext, _orgServ, statusReason, cas.LogicalName, resCasId, ticket);
+
+                        Console.WriteLine(ticket + " updated. \n");
                     }
                 }
                 catch (FaultException<OrganizationServiceFault> e)
@@ -103,55 +141,102 @@ namespace CaseAnonymisation
                     Console.WriteLine("Exception: occured on Case: " + ticket + "\n");
                     Console.WriteLine(e.Message);
                 }
-                _orgContext?.Dispose();
-                retrievedCases.Entities.Clear();
+                retrievedResCases.Entities.Clear();
             }
             else
             {
-                return; 
+                return;
             }
             goto start;
         }
 
-        public static void activityUpdate(IOrganizationService _orgServ, Guid CaseId)
+        public static void OpenResolvedCase(IOrganizationService _orgServ, Guid resCasId, string ticket)
+        {
+            try
+            {
+                SetStateRequest state = new SetStateRequest();
+                state.EntityMoniker = new EntityReference("incident", resCasId);
+                state.State = new OptionSetValue(0);
+                state.Status = new OptionSetValue(1);
+                SetStateResponse stateSet = (SetStateResponse)_orgServ.Execute(state);
+            }
+            catch (FaultException<OrganizationServiceFault> e)
+            {
+                Console.WriteLine("Exception: occured on Case: " + ticket + "\n" + e.Message);
+            }
+        }
+
+        public static void CloseCase(OrganizationServiceContext orgContext, IOrganizationService _orgServ, int statusReason, string logiName, Guid incId, string ticket)
+        {
+            if (orgContext != default(OrganizationServiceContext))
+            {
+
+                var incidentResolution = new Entity("incidentresolution");
+                incidentResolution["subject"] = "Anonymised Incident Resolved";
+                incidentResolution["incidentid"] = new EntityReference(Incident.EntityLogicalName, incId);
+                incidentResolution["description"] = "Test Case";
+                incidentResolution["timespent"] = 60;
+
+                var closeIncidentRequest = new CloseIncidentRequest
+                {
+                    IncidentResolution = incidentResolution,
+                    Status = new OptionSetValue(statusReason)
+                };
+
+                try
+                {
+                    _orgServ.Execute(closeIncidentRequest);
+                }
+                catch (Exception ex)
+                {
+                    string errorMsg = ticket + ex.StackTrace;
+                }
+                
+            }
+        }
+       
+        public static void activityUpdate(IOrganizationService _orgServ, Guid CaseId, string ticket)
         {
             OrganizationServiceContext _orgContext = new OrganizationServiceContext(_orgServ);
             EntityCollection retrievedActivities = GetActivities(_orgServ, CaseId);
 
             foreach (Entity entity in retrievedActivities.Entities)
             {
-                string logicalName = entity.Attributes["activitytypecode"].ToString();
-                Console.Write("Updating Activity Type: " + logicalName.ToString() + ", \n");
-                Guid emailid = (Guid)entity.Attributes["activityid"];
-                Entity activity = new Entity();
+                var logicalName = entity["activitytypecode"].ToString();
+                var typeCode = entity["activitytypecode"].ToString();
+                var actId = (Guid)entity["activityid"];
 
+                var updateAct = new Entity
+                {
+                    Id = actId,
+                    LogicalName = logicalName
+                };
+
+                #region
                 if (logicalName == "email")
                 {
                     SetStateRequest setStateRequest = new SetStateRequest()
                     {
                         EntityMoniker = new EntityReference
                         {
-                            Id = emailid,
+                            Id = actId,
                             LogicalName = logicalName,
                         },
                         Status = new OptionSetValue(1),
                         State = new OptionSetValue(0)
                     };
                     _orgServ.Execute(setStateRequest);
-                    goto Update;
                 }
-            Update:
-                activity.LogicalName = logicalName;
-                activity.Id = new Guid(entity.Attributes["activityid"].ToString());
-                activity.Attributes["description"] = "XXXX XXXX XXXX XXX";
-                activity.Attributes["subject"] = "XXXX XXXX XXXX XXXX";
+                #endregion
 
-                _orgContext.Attach(activity);
-                _orgContext.UpdateObject(activity);
+                updateAct["description"] = "Anonymised activity description. " + typeCode;
+                updateAct["subject"] = "Activity type: " + typeCode;
+
+
+                _orgServ.Update(updateAct);
             }
-            _orgContext.SaveChanges();
+            Console.WriteLine("Activities for " + ticket + " have been updated \n");
 
-            _orgContext?.Dispose();
             retrievedActivities.Entities.Clear();
         }
     }
